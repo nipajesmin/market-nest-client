5b9286baf5604d60cb2cc83fcef8c7d8d2c63c3e
nipajesmin <nipakterjesmin@gmail.com>
2025-04-21 12:09:00 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='bg-red-700'>Hello </h1>
+      <button class="btn btn-primary">Button</button>
+      
+    </>
+  )
+}
+
+export default App
